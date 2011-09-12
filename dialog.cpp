5e75397cd34c0c7613759f6f519315bbd1f92eef
b.safin <b.safin@bftcom.com>
2011-09-12 11:43:09 +0400

    Выравнивание контролов

--- dialog.cpp
+++ dialog.cpp
@@ -2,13 +2,17 @@
 #include "ui_dialog.h"
 #include <QSettings>
 
+#define REG_KEY "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment"
+#define REG_VAR "Path"
+
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
-    ui->setupUi(this);
-    QSettings settings("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", QSettings::NativeFormat);
-    QString S = settings.value("Path").toString();
+    ui->setupUi(this);    
+
+    QSettings settings(REG_KEY, QSettings::NativeFormat);
+    QString S = settings.value(REG_VAR).toString();
     ui->plainTextEdit->document()->setPlainText(S.replace(";","\n"));
 }
 
